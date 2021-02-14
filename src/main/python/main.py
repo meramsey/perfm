0797ec265be366d36f8e302389f51585c8b8dab1
Michael Ramsey <mike@hackerdise.me>
2021-02-13 22:01:31 -0500

    Setup permissions labels and fields

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -2,6 +2,7 @@
 # from fbs_runtime.excepthook.sentry import SentryExceptionHandler
 import os
 import os.path
+import stat
 from pathlib import Path
 import sys
 # import requests
@@ -13,14 +14,58 @@ from PyQt5.QtWidgets import *
 from fbs_runtime.application_context.PyQt5 import ApplicationContext, \
     cached_property
 
-# Import your permissions file and class
-from permission import *
-
 # example of dynamically loading sqlite from cwd of installer
+
 target_db = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), "somesqlite.db")
 
 home_dir = os.path.expanduser("~")
 somefile = Path(os.path.join("~/.bashrc"))
+
+
+def octal_permission_digit(rwx):
+    digit = 0
+    if rwx[0] == 'r':
+        digit += 4
+    if rwx[1] == 'w':
+        digit += 2
+    if rwx[2] == 'x':
+        digit += 1
+    return digit
+
+
+class FilePerm:
+    def __init__(self, filepath):
+        filemode = stat.filemode(os.stat(filepath).st_mode)
+        permission_group = [filemode[-9:][i:i + 3] for i in range(0, len(filemode[-9:]), 3)]
+        self.filepath = filepath
+        self.permissions = dict(zip(['user', 'group', 'other'], [list(p) for p in permission_group]))
+
+    def get_mode(self):
+        octal_digits = [octal_permission_digit(p) for p in self.permissions.values()]
+        mode = 0
+        for shift, octal in enumerate(octal_digits[::-1]):
+            mode += octal << (shift * 3)
+        return mode
+
+    def update(self, access, read=None, write=None, execute=None):
+        if access in self.permissions.keys():
+            perm = self.permissions[access]
+            if read is not None:
+                if read:
+                    perm[0] = 'r'
+                else:
+                    perm[0] = '-'
+            if write is not None:
+                if write:
+                    perm[1] = 'w'
+                else:
+                    perm[1] = '-'
+            if execute is not None:
+                if execute:
+                    perm[2] = 'x'
+                else:
+                    perm[2] = '-'
+            os.chmod(self.filepath, self.get_mode())
 
 
 class AppContext(ApplicationContext):
@@ -140,10 +185,31 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # os.startfile(file_path)
         file = QFileInfo(file_path)
         file_modified = file.lastModified().toString()
-        print(str(file.permissions))
         self.file_name_info.setText(file.canonicalFilePath())
         self.file_size_info.setText(str(file.size()))
         self.file_changed_info.setText(str(file_modified))
+        perms = FilePerm(file_path)
+        user_perms = perms.permissions['user']
+        group_perms = perms.permissions['group']
+        other_perms = perms.permissions['other']
+        self.permissions_info_user.setText(''.join(user_perms))
+        self.permissions_info_group.setText(''.join(group_perms))
+        self.permissions_info_other.setText(''.join(other_perms))
+        print(vars(perms))
+        (perms.__dict__.keys())
+        # att_perms = [attr for attr in dir(perms()) if not callable(getattr(perms(), attr)) and not attr.startswith("__")]
+        #print(att_perms)
+        print(str(perms.get_mode()))
+        print(QFile(file_path))
+        print(file.permissions())
+        print(str(QFile(file_path)))
+        print()
+        print('File: %s' % file.filePath())
+        print('')
+        print('Qt Writable: %s' % file.isWritable())
+        print('Qt Permission: %s' % bool(file.permissions() & QFile.WriteUser))
+        print('Py Writable: %s' % os.access(file_path, os.W_OK))
+        print('Py Permission: %s' % bool(os.stat(file_path).st_mode & stat.S_IWUSR))
 
 
 if __name__ == '__main__':
